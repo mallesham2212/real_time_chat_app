b8bc1aef329e0aefdafb0a0b58e852054b25b688
mallesham2212 <malleshamprojects22@gmail.com>
2025-10-08 15:55:55 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ app.use("/api/messages", messageRouter);
 // Serve frontend in production
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
 
   app.use(express.static(frontendPath));
 
